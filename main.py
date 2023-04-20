cf85903a83c22adeb752044061fe4ec02bea54aa
Gregory <99868477+M666abc@users.noreply.github.com>
2023-04-20 08:46:53 +0200

    Update main.py
    still doesn't work, need fixing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+import random 
 import time
 
 num_values = int(input("Enter number of notes to generate: "))
@@ -8,26 +8,27 @@ jump_frequency = int(input("Enter jump frequency (every nth note): "))
 
 # generate an initial random value
 lane_values = [random.randint(1, 4)]
-previous_lane = lane_values[-1]
+previous_lanes = lane_values[-4:]
 
 for i in range(1, num_values):
     if i % jump_frequency == 0:
         # add jump
-        jump_lane = random.choice([lane for lane in range(1, 5) if lane != previous_lane])
+        jump_lane = random.choice([lane for lane in range(1, 5) if lane not in previous_lanes])
         lane_values.append(jump_lane)
-        print("- StartTime: " + str(start_time) + "\n  Lane: " + str(previous_lane) + "\n  KeySounds: []")
+        print("- StartTime: " + str(start_time) + "\n  Lane: " + str(previous_lanes[-1]) + "\n  KeySounds: []")
         start_time += interval
         print("- StartTime: " + str(start_time) + "\n  Lane: " + str(jump_lane) + "\n  KeySounds: []")
-        previous_lane = jump_lane
+        previous_lanes.pop(0)
+        previous_lanes.append(jump_lane)
     else:
         # generate a random value that is different from the previous value
         available_lanes = [1, 2, 3, 4]
-        available_lanes.remove(previous_lane)
-        if len(lane_values) > 1 and len(available_lanes) > 1:
-            # avoid consecutive repeats by removing the previous value from the available lanes
-            available_lanes.remove(lane_values[-2])
+        for lane in previous_lanes:
+            if lane in available_lanes:
+                available_lanes.remove(lane)
         lane_value = random.choice(available_lanes)
         lane_values.append(lane_value)
         print("- StartTime: " + str(start_time) + "\n  Lane: " + str(lane_value) + "\n  KeySounds: []")
-        previous_lane = lane_value
+        previous_lanes.pop(0)
+        previous_lanes.append(lane_value)
         start_time += interval
